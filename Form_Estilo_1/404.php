4ce7d3ded0d9fc29c7ae6163f539d0a8948a7e98
root <root@localhost.localdomain>
2019-08-23 14:57:46 -0300

    renamed directories

---
+++ Form_Estilo_1/404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>404</title>
+	<style type="text/css">
+		body{
+			background-image: url(wallpaper.jpg);
+			text-align: center;
+			font-family: Blippo, fantasy;
+		}
+
+		body a{
+			text-decoration: none;
+		}
+
+		.center{
+			position: relative;
+			top: 200px;
+          	color: #332d00;
+		}
+
+		#back button{
+			background-color: #332d00;
+			color: #FBDBA8;
+			border: 5px solid #332d00;
+		}
+		#back button:hover{
+			background-color: #FBDBA8;
+			color: #332d00;
+			border: 5px solid #FBDBA8;
+		}
+	</style>
+</head>
+<body>
+	<h1 class='center'>ERROR 404</h1>
+	<h2 class='center'>PÁGINA NÃO ENCONTRADA</h2>
+	<p><a href="index.html" class="center"><span id="back"><button>VOLTAR</button></span></a></p>
+</body>
+</html>
